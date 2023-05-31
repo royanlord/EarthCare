437c3069dcf1c3106b64c0ea0b3dd847e3220ee4
Ghulsan Savana Ahasya <ghulsan.savanaa@gmail.com>
2023-05-31 17:52:38 +0700

    create content About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,65 @@
-function About(){
-    return(
-        <>
-        <div>
-            <h1>About Page</h1>
-        </div>
+function About() {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <img src="" alt="" />
+              <p>Image</p>
+            </div>
+            <div className="col-lg-6">
+              <p className="fs-3 fw-bold">
+                EarthCare, <br />
+                <span className="fs-1">What is it?</span>
+              </p>
+              <p>
+                EarthCare adalah suatu platform aplikasi berbasis website yang
+                berfokus pada kepedulian dan kelestarian alam dimana pada
+                mulanya terbentuk karena didasari atas rasa prihatin terhadap
+                Bumi yang kian rapuh. Greenpeace sebagai wadah dan tempat untuk
+                mengedukasi dan mengajak masyarakat banyak agar lebih peduli
+                terhadap kelestarian alam dan juga turut serta untuk menjaganya.
+              </p>
+              <p>Memberi Solusi. Gerakkan Aksi. Ciptakan Perubahan.</p>
+            </div>
+          </div>
 
-        </>
-    )
+          <div className="row my-5">
+            <div className="col-lg-6">
+              <p className="fs-3 fw-bold">
+                Goals, <br />
+                <span className="fs-1">To Be Achieved</span>
+              </p>
+              <ol>
+                <li>Mengajak masyarakat untuk menjaga kelestarian alam</li>
+                <li>
+                  Mengedukasi masyarakat tentang pentingnya kelestarian alam
+                </li>
+                <li>
+                  Menumbuhkan rasa kepedulian masyarakat terhadap alam dan
+                  ekosistemnya
+                </li>
+                <li>
+                  Sebagai penggerak masyarakat untuk bersama memperbaiki
+                  kerusakan alam yang sudah terjadi
+                </li>
+                <li>
+                  Menjaga agar Bumi yang kian rapuh ini tetap mempunyai
+                  kemampuan untuk menopang kehidupan seluruh makhluk hidup
+                </li>
+                <li>Menciptakan masa depan yang bebas bahan racun</li>
+              </ol>
+            </div>
+            <div className="col-lg-6">
+              <img src="" alt="" />
+              <p>Image</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default About
+export default About;
