a0af43b4136d6a64cf94b684c97b2929e1c0c002
royanlord <royanfarid32@gmail.com>
2023-06-01 15:54:35 +0700

    import Article to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./css/style.css";
+import { Article } from "./pages/Article";
 
 function App() {
   return (
     <>
-      <h1>Hello, React JS</h1>
+      {/* <h1>Hello, React JS</h1> */}
+      <Article />
     </>
   );
 }
