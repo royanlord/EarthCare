8c50359377de4dc5725a0cb8e4c039b83469fd72
royanlord <royanfarid32@gmail.com>
2023-06-01 11:06:44 +0700

    add Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import "./css/style.css";
+import Event from "./pages/Event";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <>
+      <Routes>
+        {/* <Route path="/login" element={<Login />}></Route> */}
+        {/* <Route path="/" element={<Home />}></Route> */}
+        {/* <Route path="/event" element={<Event />} /> */}
+        {/* <Route path="/article" element={<Article />}></Route> */}
+        {/* <Route path="/about" element={<About />}></Route> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
       {/* <h1>Hello, React JS</h1> */}
-      <Navbar />
+      {/* <Navbar /> */}
     </>
   );
 }
