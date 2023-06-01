42d3a8ecd186277d60d468dc3f2825c8df38c97e
royanlord <royanfarid32@gmail.com>
2023-06-01 11:08:19 +0700

    change a tag to react router Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import "../css/navbar.css"
 import { Dropdown } from "bootstrap"
 
@@ -7,10 +8,10 @@ export const Navbar = () => {
             <header>
                 <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" data-bs-theme="dark">
                     <div className="container">
-                        <a className="navbar-brand text-white" href="index.html">
+                        <Link className="navbar-brand text-white" to="/">
                             {/* <img src="" className="logo-nav" alt="logo navbar" height="50" /> */}
                             EarthCare
-                        </a>
+                        </Link>
                         <button 
                             className="navbar-toggler" 
                             type="button" 
@@ -25,7 +26,7 @@ export const Navbar = () => {
                         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-lg-center text-center">
                                 <li className="nav-item">
-                                    <a className="nav-link" aria-current="page" href="index.html">Home</a>
+                                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
                                 </li>
                                 <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
@@ -34,26 +35,26 @@ export const Navbar = () => {
                                     </a>
                                     <ul className="dropdown-menu animate slideIn dropdown-menu-dark">
                                         <li>
-                                            <a className="dropdown-item" href="event.html">Event</a>
+                                            <Link className="dropdown-item" to="/event">Event</Link>
                                         </li>
                                         <li>
                                             <hr className="dropdown-divider" />
                                         </li>
                                         <li>
-                                            <a className="dropdown-item" href="article.html">Article</a>
+                                            <Link className="dropdown-item" to="/article">Article</Link>
                                         </li>
                                     </ul>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" aria-current="page" href="about.html">About</a>
+                                    <Link className="nav-link" aria-current="page" to="/about">About</Link>
                                 </li>
                                 <li className="nav-item me-lg-4">
-                                    <a className="nav-link active" href="#">Contact Us</a>
+                                    <Link className="nav-link active" to="/contact">Contact Us</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="login.html">
+                                    <Link className="nav-link" to="/login">
                                         <button className="btn btn-primary px-3 border-0" type="submit">Login</button>
-                                    </a>
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
