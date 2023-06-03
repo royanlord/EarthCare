9378a981446d3d61471fabc3ae2032f125c551d5
royanlord <royanfarid32@gmail.com>
2023-06-03 21:17:32 +0700

    import Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./css/style.css";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <h1>Hello, React JS</h1>
+      {/* <h1>Hello, React JS</h1> */}
+      <Contact />
     </>
   );
 }
