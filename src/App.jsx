fd613ff7f94d923a83bb794b1ec36eabafa4e13d
royanlord <royanfarid32@gmail.com>
2023-06-03 11:45:15 +0700

    modified App n main jsx file, add router react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./css/style.css";
 import { Article } from "./pages/Article";
+import { DetailArticle } from "./pages/DetailArticle";
 
 function App() {
   return (
     <>
       {/* <h1>Hello, React JS</h1> */}
-      <Article />
+      {/* <Article /> */}
+      <Routes>
+        <Route path="/" element={<Article />} />
+        <Route path="/detailArticle/:id" element={<DetailArticle />} />
+      </Routes>
     </>
   );
 }
