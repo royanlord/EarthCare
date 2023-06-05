91c9ddfa053dda505794ab07ab440ab021d72016
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-06-05 11:39:52 +0700

    update state detail events

--- src/pages/DetailEvents.jsx
+++ src/pages/DetailEvents.jsx
@@ -11,7 +11,7 @@ import {
 
 export default function DetailEvents() {
   const { id } = useParams();
-  const [detailEvents, setDetailEvents] = useState(null);
+  const [detailEvents, setDetailEvents] = useState("");
 
   useEffect(() => {
     fetchDetailEvents();
@@ -33,75 +33,71 @@ export default function DetailEvents() {
   };
 
   if (!detailEvents) {
-    return <p className="text-center">Loading.....</p>;
+    return <p className="text-center mt-4">Loading....</p>;
   }
 
   return (
-    <div className="d-flex justify-content-center align-items-center">
+    <div className="detail__events d-flex justify-content-center align-items-center">
       <main className="d-flex container mx-5">
-        <div>
-          <Row>
-            <Col xs={12} lg={8}>
-              <div>
-                <img
-                  className="img-fluid mt-5"
-                  src={detailEvents.gambar}
-                  alt="ilustrasi detail events"
-                />
-                <h3 className="mt-2">Detail Events</h3>
-                <hr />
-                <p style={{ textAlign: "justify" }}>{detailEvents.detail}</p>
-              </div>
-            </Col>
+        <Row>
+          <Col xs={12} lg={8}>
+            <div className="card__image">
+              <img
+                className="img-fluid mt-5"
+                src={detailEvents.gambar}
+                alt="ilustrasi detail events"
+              />
+              <h3 className="mt-2">Detail Events</h3>
+              <hr color="black" />
+              <p style={{ textAlign: "justify" }}>{detailEvents.detail}</p>
+            </div>
+          </Col>
 
-            <Col xs={12} lg={4}>
-              <div className="card card-detail-event mt-5">
-                <div className="card-body">
-                  <h4 className="card-title mb-2 fw-bold">
-                    {detailEvents.judul}
-                  </h4>
-                  <Badge bg="success me-2 mb-3">Lingkungan</Badge>
-                  <Badge bg="success">Events</Badge>
-                  <div className="date-detail mb-3 mt-3">
-                    <FaCalendarAlt />
-                    <p className="d-inline text-muted ms-3">
-                      {detailEvents.tanggal}
-                    </p>
-                  </div>
-                  <div className="time-detail mb-3">
-                    <FaClock />
-                    <p className="d-inline text-muted ms-3">
-                      {detailEvents.waktu}
-                    </p>
-                  </div>
-                  <div className="ticket-detail mb-3">
-                    <FaTicketAlt />
-                    <p className="d-inline text-muted ms-3">
-                      {detailEvents.tiket}
-                    </p>
-                  </div>
-                  <div className="location-detail mb-3">
-                    <FaMapMarker />
-                    <p className="d-inline text-muted ms-3">
-                      {detailEvents.lokasi}
-                    </p>
-                  </div>
-                  <div className="registration-detail mb-3">
-                    <FaLink />
-                    <p className="d-inline text-muted ms-3">
-                      {detailEvents.pendaftaran}
-                    </p>
-                  </div>
-                  <div className="d-grid mt-2 mt-3 btn-detail-event">
-                    <a href="" className="btn btn-primary">
-                      Daftar Event
-                    </a>
-                  </div>
+          <Col xs={12} lg={4}>
+            <div className="card card__detail mt-5">
+              <div className="card-body">
+                <h4 className="card-title mb-2">{detailEvents.judul}</h4>
+                <Badge bg="success me-2 mb-3">Lingkungan</Badge>
+                <Badge bg="success">Events</Badge>
+                <div className="date-detail mb-3 mt-3">
+                  <FaCalendarAlt />
+                  <p className="d-inline text-muted ms-3">
+                    {detailEvents.tanggal}
+                  </p>
+                </div>
+                <div className="time-detail mb-3">
+                  <FaClock />
+                  <p className="d-inline text-muted ms-3">
+                    {detailEvents.waktu}
+                  </p>
+                </div>
+                <div className="ticket-detail mb-3">
+                  <FaTicketAlt />
+                  <p className="d-inline text-muted ms-3">
+                    {detailEvents.tiket}
+                  </p>
+                </div>
+                <div className="location-detail mb-3">
+                  <FaMapMarker />
+                  <p className="d-inline text-muted ms-3">
+                    {detailEvents.lokasi}
+                  </p>
+                </div>
+                <div className="registration-detail mb-3">
+                  <FaLink />
+                  <p className="d-inline text-muted ms-3">
+                    {detailEvents.pendaftaran}
+                  </p>
+                </div>
+                <div className="d-grid mt-2 mt-3 btn-detail-event">
+                  <a href="" className="btn btn-primary">
+                    Daftar Event
+                  </a>
                 </div>
               </div>
-            </Col>
-          </Row>
-        </div>
+            </div>
+          </Col>
+        </Row>
       </main>
     </div>
   );
