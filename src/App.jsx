24a39801f414b2df33e570897469156c33d5b688
royanlord <royanfarid32@gmail.com>
2023-06-06 21:47:52 +0700

    update article app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./css/style.css";
-import { Article } from "./pages/Article";
-import { DetailArticle } from "./pages/DetailArticle";
 
 function App() {
   return (
     <>
-      {/* <h1>Hello, React JS</h1> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Article />} />
-          <Route path="/detailArticle/:id" element={<DetailArticle />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>Hello, React JS</h1>
     </>
   );
 }
