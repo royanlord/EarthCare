6650bc0a921346ffed8231bb961a3c68f273e486
royanlord <royanfarid32@gmail.com>
2023-06-06 20:51:41 +0700

    move browserrouter from main to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import { Contact } from "./pages/Contact";
 function App() {
   return (
     <>
-      <Routes>
-        {/* <Route path="/login" element={<Login />}></Route> */}
-        {/* <Route path="/" element={<Home />}></Route> */}
-        {/* <Route path="/event" element={<Event />} /> */}
-        {/* <Route path="/article" element={<Article />}></Route> */}
-        {/* <Route path="/about" element={<About />}></Route> */}
-        {/* <Route path="/contact" element={<Contact />} /> */}
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/login" element={<Login />}></Route> */}
+          {/* <Route path="/" element={<Home />}></Route> */}
+          {/* <Route path="/event" element={<Event />} /> */}
+          {/* <Route path="/article" element={<Article />}></Route> */}
+          {/* <Route path="/about" element={<About />}></Route> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </BrowserRouter>
       {/* <h1>Hello, React JS</h1> */}
       {/* <Navbar /> */}
     </>
