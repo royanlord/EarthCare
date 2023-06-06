c8907f60b40d1a66b238f1aa31eff4c4a3bf5690
royanlord <royanfarid32@gmail.com>
2023-06-06 21:37:37 +0700

    update contact app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./css/style.css";
-import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <>
-      {/* <h1>Hello, React JS</h1> */}
-      <Contact />
+      <h1>Hello, React JS</h1>
     </>
   );
 }
