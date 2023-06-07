0178da22e11bb2cb9b52263ce207fc1f8a5d31fa
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-06-07 19:40:26 +0700

    fix updated development

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-// import "./css/style.css";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Events from "./pages/Events";
-// import DetailEvents from "./pages/DetailEvents";
-// import DaftarEvents from "./pages/DaftarEvents";
-
 function App() {
   return (
     <>
-      {/* Testing */}
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Events />} />
-          <Route path="/events/:id" element={<DetailEvents />} />
-          <Route path="/daftarevents/:id" element={<DaftarEvents />}></Route>
-        </Routes>
-      </BrowserRouter> */}
       <h1>Hello, React JS</h1>
     </>
   );
