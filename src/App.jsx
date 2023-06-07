590e31f5476da5c3191a5d56594b16968419da4f
royanlord <royanfarid32@gmail.com>
2023-06-07 12:51:37 +0700

    update app in auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./css/style.css";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <>
-      <h1>Hello, React JS</h1>
+      {/* <h1>Hello, React JS</h1> */}
+      {/* <Register /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
