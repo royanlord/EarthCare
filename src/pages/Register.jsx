c2e72383e73e5cbea5fe31777a7f749e25b5c96d
royanlord <royanfarid32@gmail.com>
2023-06-07 12:36:14 +0700

    create register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,57 @@
+import "../css/auth.css"
+import authImg from "../assets/auth.gif"
+import { useLayoutEffect } from "react"
+import { Link } from "react-router-dom"
+
 export const Register = () => {
-    
+    useLayoutEffect(() => {
+        document.body.style.backgroundColor = "#115810"
+    })
+
+    return (
+        <>
+            <div class="container mt-5">
+                <div class="row mx-4">
+                    <div class="col-md-6">
+                        <img class="img-fluid mb-sm-3" src={authImg} alt="LottieFiles Register" />
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <h1 class="text-center mb-5 title-login">register</h1>
+                        <form>
+                            <div class="mb-4">
+                                <input type="name" class="form-control form-control-lg" name="fullName" id="fullName"
+                                    placeholder="Fullname" autocomplete="off" />
+                            </div>
+                            <div class="mb-4">
+                                <input type="email" class="form-control form-control-lg" name="emailAddress" id="emailAddress"
+                                    placeholder="Email" autocomplete="off" />
+                            </div>
+                            <div class="mb-4">
+                                <input type="password" class="form-control form-control-lg" name="passwordAddress"
+                                    id="passwordAddress" placeholder="Password" />
+                            </div>
+                            <div class="d-grid">
+                                <button 
+                                    id="btn_register" 
+                                    class="btn btn-success btn-lg" 
+                                    // onclick="dataAuthRegister()"
+                                >
+                                    Register
+                                </button>
+                                <span class="text-center mt-4">
+                                    Do you have an account?&nbsp; 
+                                    <Link class="text-register" to="/login">
+                                        Login in here
+                                    </Link>
+                                    {/* <a href="login.html" class="text-register">
+                                        Login in here
+                                    </a> */}
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
