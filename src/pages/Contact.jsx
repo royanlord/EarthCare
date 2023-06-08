9605aeb2de35c1864181bd2525ae6e619e5b698c
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-06-08 18:47:32 +0700

    updated page contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,23 @@
-import { ContactForm } from "../components/ContactForm"
-import "../css/contact.css"
+import { ContactForm } from "../components/ContactForm";
+import "../css/contact.css";
 
 export const Contact = () => {
-    return (
-        <>
-            <main>
-                <section id="jumbotron-contact">
-                    <div class="container-fluid bg-dark d-flex justify-content-center align-items-center">
-                        <div class="row">
-                            <div class="col-7 mx-auto">
-                                <h1 class="text-white text-center text-wrap fw-semibold display-5">Contact Us About EarthCare Website</h1>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <ContactForm />
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      <main>
+        <section id="jumbotron-contact">
+          <div className="container-fluid bg-dark d-flex justify-content-center align-items-center">
+            <div className="row">
+              <div className="col-7 mx-auto">
+                <h1 className="text-white text-center text-wrap fw-semibold display-5">
+                  Contact Us About EarthCare Website
+                </h1>
+              </div>
+            </div>
+          </div>
+        </section>
+        <ContactForm />
+      </main>
+    </>
+  );
+};
