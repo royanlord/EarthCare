f9edbf215788706a0b6cbb235b9d8565f1fb2eec
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-06-12 22:15:24 +0700

    update app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import "./css/style.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Events from "./pages/Events";
-import DetailEvents from "./pages/DetailEvents";
-import DaftarEvents from "./pages/DaftarEvents";
-
 function App() {
   return (
     <>
-      {/* Testing */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Events />} />
-          <Route path="/events/:id" element={<DetailEvents />} />
-          <Route path="/daftarevents/:id" element={<DaftarEvents />}></Route>
-        </Routes>
-      </BrowserRouter>
-      {/* <h1>Hello, React JS</h1> */}
+      <h1>Hello, React JS</h1>
     </>
   );
 }
