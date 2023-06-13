043f5325f3c606dcf55f1c8ebe61533de7afbcf3
royanlord <royanfarid32@gmail.com>
2023-06-14 01:47:53 +0700

    import article provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LoginProvider from "./context/LoginProvider.jsx";
 import UserProvider from "./context/UserProvider.jsx";
+import ArticlesProvider from "./context/ArticlesProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
     <LoginProvider>
       <UserProvider>
-        <App />
+        <ArticlesProvider>
+          <App />
+        </ArticlesProvider>
       </UserProvider>
     </LoginProvider>
   // </React.StrictMode>
