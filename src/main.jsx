3a3ec8744037d5ba8b07ca37810710f236603f26
royanlord <royanfarid32@gmail.com>
2023-06-14 08:18:24 +0700

    import events provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import LoginProvider from "./context/LoginProvider.jsx";
 import UserProvider from "./context/UserProvider.jsx";
 import ArticlesProvider from "./context/ArticlesProvider.jsx";
+import EventsProvider from "./context/EventsProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
     <LoginProvider>
       <UserProvider>
         <ArticlesProvider>
-          <App />
+          <EventsProvider>
+            <App />
+          </EventsProvider>
         </ArticlesProvider>
       </UserProvider>
     </LoginProvider>
